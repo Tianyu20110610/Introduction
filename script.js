42de65b59d57e1f6fbda29752e91d8a5643e7815
Tianyu20110610 <tianyu.liu2023@gmail.com>
2025-08-30 18:44:09 +0200

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-window.onresize=screen
-window.onload=screen
+window.onresize=screen;
+window.onload=screen;
 function screen(){
     console.log("hi");
-    Width=width.innerWidth;
-    document.getElementById("size").innterHTML="width:"+Width+" px"
+    Width=window.innerWidth;
+    console.log(Width);
+    document.getElementById("size").innerHTML="width:"+Width+" px";
     
 }
